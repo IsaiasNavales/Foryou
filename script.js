25065b71a3297de4754d90e4666f3d6767e980f0
Isaias <120467912+IsaiasNavales@users.noreply.github.com>
2023-12-05 08:58:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, Thank you so much!!";
+  question.innerHTML = "ihhhh, Thank you so much!!";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
